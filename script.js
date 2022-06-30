5443c56efc878968322932333e7a4db838e46b3f
ChewingTabasco <emerickjustice@gmail.com>
2022-06-30 12:29:50 -0400

    Render 16*16 grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const gridContainer = document.querySelector("#grid-container");
+let gridWidth = 16;
+const gridArea = gridWidth * gridWidth;
+
+for (let i = 1; i <= gridArea; i++) {
+  let gridTile = document.createElement("div");
+  //   gridContainer.appendChild(document.createElement("div"));
+
+  gridContainer.appendChild(gridTile);
+  gridTile.classList.add("grid-tile");
+}
