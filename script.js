37388f0521c442280277bb898dc46f956994b306
ChewingTabasco <emerickjustice@gmail.com>
2022-06-30 12:53:53 -0400

    Change background-color of gridTile on hover

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const gridContainer = document.querySelector("#grid-container");
 let gridWidth = 16;
 const gridArea = gridWidth * gridWidth;
+let gridTile;
 
 for (let i = 1; i <= gridArea; i++) {
-  let gridTile = document.createElement("div");
+  gridTile = document.createElement("div");
   gridContainer.appendChild(gridTile);
   gridTile.classList.add(`grid-tile${gridWidth}`);
+
+  gridTile.addEventListener("mouseover", function (e) {
+    e.target.setAttribute("style", "background-color: blue;");
+  });
 }
