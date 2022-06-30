f83f69840bc69f5ffce28eaba63b7d54fed3805e
ChewingTabasco <emerickjustice@gmail.com>
2022-06-30 12:36:33 -0400

    Clean up code

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const gridArea = gridWidth * gridWidth;
 
 for (let i = 1; i <= gridArea; i++) {
   let gridTile = document.createElement("div");
-  //   gridContainer.appendChild(document.createElement("div"));
-
   gridContainer.appendChild(gridTile);
   gridTile.classList.add(`grid-tile${gridWidth}`);
 }
